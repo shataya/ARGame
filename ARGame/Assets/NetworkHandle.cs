f1eac2e3741528d67a5ca592fc21405ebe797bdb
shataya <juelide.Meyer@Gmail.com>
2015-11-10 19:44:24 +0100

    EventHandler für Server

---
+++ ARGame/Assets/NetworkHandle.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System;
+
+public class NetworkHandle : NetworkManager {
+
+    public override void OnServerConnect(NetworkConnection conn)
+    {
+        base.OnServerConnect(conn);
+
+        conn.RegisterHandler(MsgType.Ready, OnReady);
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+
+       
+    }
+
+    private void OnReady(NetworkMessage netMsg)
+    {
+        Debug.Log("HALLO");
+       
+        Debug.Log(netMsg.reader.ReadString());
+    }
+}
