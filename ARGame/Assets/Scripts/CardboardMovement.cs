64f6d4452212b7c9806032340f0aaf11b127556b
Klaus Bleck <klaus.bleck@gmail.com>
2015-11-29 16:27:30 +0100

    VR Template eingebaut Restrukturierung

---
+++ ARGame/Assets/Scripts/CardboardMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using System.Collections;
+
+public class CardboardMovement : MonoBehaviour
+{
+    public float movementSpeed = 3.0f;
+
+    private const string HORIZONTAL_NAME = "Horizontal";
+    private const string VERTICAL_NAME = "Vertical";
+
+    private float vertical;
+    private float horizontal; 
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        horizontal = CrossPlatformInputManager.GetAxis (HORIZONTAL_NAME);
+        vertical = CrossPlatformInputManager.GetAxis (VERTICAL_NAME);
+
+        this.transform.Translate ((Vector3.forward * vertical + Vector3.right * horizontal) * Time.deltaTime * movementSpeed);
+	}
+}
