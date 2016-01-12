50fd126c1c69443743e04cc1995ccb947688da40
Klaus Bleck <klaus.bleck@gmail.com>
2016-01-12 18:18:45 +0100

    Regeneration und Fixes

---
+++ ARGame/Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public float currentHealth;
+    public float completeHealth = 100.0f;
+    public Vector3 spawnPoint = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+        currentHealth = completeHealth;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (currentHealth <= completeHealth)
+        {
+            // Regeneriere
+            currentHealth = Mathf.Lerp (currentHealth, completeHealth, 0.00025f * Time.deltaTime);
+            if (currentHealth > completeHealth)
+            {
+                currentHealth = completeHealth;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag.Equals("EnergyBall"))
+        {           
+            EnergyBall eb = collision.gameObject.GetComponent<EnergyBall> ();
+            if(eb != null)
+            {
+                Debug.Log ("hit by energyball");
+                currentHealth -= eb.damage;
+                if (currentHealth <= 0.0f)
+                    Die ();
+            }
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log ("gestorben");
+    }
+}
